400291df1e962574aa45ac5060539669e0981c03
CtrlAltDelete4413 <95325881+CtrlAltDelete4413@users.noreply.github.com>
2025-01-05 13:21:02 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'sobing4413'
+
+description 'insurance system'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/impound.lua'
+}
